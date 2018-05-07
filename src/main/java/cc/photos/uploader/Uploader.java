201d9b3b690d7a2b87254611d6489d7e6bbeff84
Edward Bridges <github@eqbridges.com>
2018-05-07 10:43:07 -0400

    logger setup

---
+++ src/main/java/cc/photos/uploader/Uploader.java
@@ -0,0 +1,11 @@
+package cc.photos.uploader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Uploader {
+  private static final Logger LOG = LoggerFactory.getLogger(Uploader.class);
+  public static void main(String[] args) {
+    LOG.debug("hello");
+  }
+}
